42a9d6d7e5b564ffbcf93b29d0eded9251a00fad
Leshon Johnson-Hampton <leshonhampton@yahoo.com>
2021-09-21 01:31:47 -0400

    Completed the script.js file for the JavaScript Basics Exercise.

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1.
+console.log(27);
+// 2.
+// Numbers, Strings, Booleans, Null, Undefined
+// 3.
+// " + , - , * , / , ** , % "
+// 4.
+console.log(1%5);
+console.log(5%2);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "Eye");
+console.log("LeShon Johnson Hampton".length);
+console.log("Avengers: Endgame"[16]);
